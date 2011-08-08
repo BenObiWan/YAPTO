ab97b23819c4b9287e5ab1557a6ba47c17aa74ba
BenObiWan <benobiwan@gmail.com>
2011-08-08 23:48:11 +0200

    Added IDataSource. Added getId and getTagList to IPicture.

---
+++ src/yapto_core/src/yapto/datasource/IDataSource.java
@@ -0,0 +1,20 @@
+package yapto.datasource;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface IDataSource extends IPictureList
+{
+	/**
+	 * Add an {@link IPicture} to the {@link IDataSource}.
+	 * 
+	 * @param picturePath
+	 *            the path to the {@link IPicture} to add.
+	 * @throws FileNotFoundException
+	 *             if the file isn't found.
+	 * @throws IOException
+	 *             if an IO error occurs during the calculation of the checksum.
+	 */
+	void addPicture(File picturePath) throws FileNotFoundException, IOException;
+}
--- src/yapto_core/src/yapto/datasource/IPicture.java
+++ src/yapto_core/src/yapto/datasource/IPicture.java
@@ -1,6 +1,20 @@
 package yapto.datasource;
 
+import java.util.List;
+
 public interface IPicture
 {
+	/**
+	 * Get the id of this {@link IPicture}.
+	 * 
+	 * @return the id of this {@link IPicture}.
+	 */
+	String getId();
 
+	/**
+	 * Get the list of {@link ITag}s associated with this {@link IPicture}.
+	 * 
+	 * @return the list of {@link ITag}s associated with this {@link IPicture}.
+	 */
+	List<ITag> getTagList();
 }
