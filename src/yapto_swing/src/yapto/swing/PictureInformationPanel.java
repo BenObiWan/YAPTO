6aad492b0c4a156911990a0644bba3ee3d3377b6
BenObiWan <benobiwan@gmail.com>
2011-08-13 11:29:40 +0200

    Added PictureInformationPanel.

---
+++ src/yapto_swing/src/yapto/swing/PictureInformationPanel.java
@@ -0,0 +1,58 @@
+package yapto.swing;
+
+import javax.swing.JPanel;
+import javax.swing.JTree;
+
+import yapto.datasource.IPicture;
+import yapto.datasource.ITag;
+
+/**
+ * Panel displaying the information of an {@link IPicture}.
+ * 
+ * @author benobiwan
+ * 
+ */
+public class PictureInformationPanel extends JPanel
+{
+	/**
+	 * serialVersionUID for Serialization.
+	 */
+	private static final long serialVersionUID = -7799432118702150952L;
+
+	/**
+	 * {@link IPicture} from which the information displayed originate.
+	 */
+	private IPicture _displayedPicture;
+
+	/**
+	 * Object used to control the access on the displayed picture.
+	 */
+	private final Object _lockPicture = new Object();
+
+	/**
+	 * Tree used to display the list of {@link ITag}s.
+	 */
+	private final JTree _tagTree = new JTree();
+
+	/**
+	 * Creates a new PictureInformationPanel.
+	 */
+	public PictureInformationPanel()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Change the {@link IPicture} whose information are displayed.
+	 * 
+	 * @param picture
+	 *            the new {@link IPicture} to handle.
+	 */
+	public void setDisplayedPicture(IPicture picture)
+	{
+		synchronized (_lockPicture)
+		{
+			_displayedPicture = picture;
+		}
+	}
+}
