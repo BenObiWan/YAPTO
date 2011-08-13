58caa7d6924c5d541cc88b4eba8bed4a6dcdec7d
BenObiWan <benobiwan@gmail.com>
2011-08-13 13:15:49 +0200

    Added getParent, getName and getDescription to ITag.

--- src/yapto_core/src/yapto/datasource/ITag.java
+++ src/yapto_core/src/yapto/datasource/ITag.java
@@ -8,5 +8,24 @@ package yapto.datasource;
  */
 public interface ITag
 {
+	/**
+	 * Get the parent of this {@link ITag}.
+	 * 
+	 * @return the parent of this {@link ITag}.
+	 */
+	ITag getParent();
 
+	/**
+	 * Get the name of this {@link ITag}.
+	 * 
+	 * @return the name of this {@link ITag}.
+	 */
+	String getName();
+
+	/**
+	 * Get the description of this {@link ITag}.
+	 * 
+	 * @return the description of this {@link ITag}.
+	 */
+	String getDescription();
 }
