771c443b1c1cb5493a656b864c2ef9abcce02ab2
BenObiWan <benobiwan@gmail.com>
2012-02-20 21:22:25 +0100

    Added SQLFileListConnection to hold the connection to the database and the prepared statement.

--- src/yapto_core/src/yapto/datasource/sqlfile/SQLFileDataSource.java
+++ src/yapto_core/src/yapto/datasource/sqlfile/SQLFileDataSource.java
@@ -4,12 +4,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -50,83 +46,6 @@ public class SQLFileDataSource implements IDataSource
 			.getLogger(SQLFileDataSource.class);
 
 	/**
-	 * Name for the 'tag' table.
-	 */
-	private static final String TAG_TABLE_NAME = "tag";
-
-	/**
-	 * Name for the 'id' column of the 'tag' table.
-	 */
-	private static final String TAG_ID_COLUMN_NAME = "id";
-
-	/**
-	 * Name for the 'name' column of the 'tag' table.
-	 */
-	private static final String TAG_NAME_COLUMN_NAME = "name";
-
-	/**
-	 * Name for the 'description' column of the 'tag' table.
-	 */
-	private static final String TAG_DESCRIPTION_COLUMN_NAME = "description";
-
-	/**
-	 * Name for the 'parentId' column of the 'tag' table.
-	 */
-	private static final String TAG_PARENT_ID_COLUMN_NAME = "parentId";
-
-	/**
-	 * Name for the 'selectable' column of the 'tag' table.
-	 */
-	private static final String TAG_SELECTABLE_COLUMN_NAME = "selectable";
-
-	/**
-	 * Name for the 'picture' table.
-	 */
-	private static final String PICTURE_TABLE_NAME = "picture";
-
-	/**
-	 * Name for the 'picture_tag' table.
-	 */
-	private static final String PICTURE_TAG_TABLE_NAME = "picture_tag";
-
-	/**
-	 * Name for the 'tagId' column of the 'picture_tag' table.
-	 */
-	private static final String PICTURE_TAG_TAG_ID_COLUMN_NAME = "tagId";
-
-	/**
-	 * Name for the 'pictureId' column of the 'picture_tag' table.
-	 */
-	private static final String PICTURE_TAG_PICTURE_ID_COLUMN_NAME = "pictureId";
-
-	/**
-	 * Connection to the database.
-	 */
-	private final Connection _connection;
-
-	/**
-	 * Statement to insert a {@link Tag}.
-	 */
-	private final PreparedStatement _psInsertTag;
-
-	/**
-	 * Statement to count the number of {@link IPicture}s having a given
-	 * {@link Tag}.
-	 */
-	private final PreparedStatement _countPicturesByTag;
-
-	/**
-	 * Statement to count the total number of {@link IPicture}s in this
-	 * {@link SQLFileDataSource}.
-	 */
-	private final PreparedStatement _countPictures;
-
-	/**
-	 * Statement to select all the {@link IPicture}s having a given {@link Tag}.
-	 */
-	private final PreparedStatement _selectPicturesByTag;
-
-	/**
 	 * Set containing all the {@link Tag}s.
 	 */
 	private final Set<Tag> _tagSet = new TreeSet<Tag>();
@@ -152,6 +71,12 @@ public class SQLFileDataSource implements IDataSource
 	private final LoadingCache<File, BufferedImage> _imageCache;
 
 	/**
+	 * Object holding the connection to the database and the prepared
+	 * statements.
+	 */
+	private final SQLFileListConnection _fileListConnection;
+
+	/**
 	 * Creates a new SQLFileDataSource.
 	 * 
 	 * @param conf
@@ -173,7 +98,8 @@ public class SQLFileDataSource implements IDataSource
 			throws SQLException, ClassNotFoundException, IOException
 	{
 		_conf = conf;
-		Class.forName("org.sqlite.JDBC");
+		_fileListConnection = new SQLFileListConnection(_conf);
+
 		final CacheLoader<String, FsPicture> pictureLoader = new FsPictureCacheLoader(
 				cacheLoaderConf);
 		final RemovalListener<String, FsPicture> pictureListener = new FsPictureRemovalListener();
@@ -185,31 +111,13 @@ public class SQLFileDataSource implements IDataSource
 
 		_imageCache = CacheBuilder.newBuilder().build(imageLoader);
 
-		_connection = DriverManager.getConnection("jdbc:sqlite:"
-				+ _conf.getDatabaseFileName());
-		_psInsertTag = _connection.prepareStatement("insert into "
-				+ TAG_TABLE_NAME + " (" + TAG_ID_COLUMN_NAME + ", "
-				+ TAG_NAME_COLUMN_NAME + ", " + TAG_DESCRIPTION_COLUMN_NAME
-				+ ", " + TAG_PARENT_ID_COLUMN_NAME + ", "
-				+ TAG_SELECTABLE_COLUMN_NAME + ") values(?, ?, ?, ?, ?)");
-		_countPicturesByTag = _connection.prepareStatement("select count("
-				+ PICTURE_TAG_PICTURE_ID_COLUMN_NAME + ") FROM "
-				+ PICTURE_TAG_TABLE_NAME + " WHERE "
-				+ PICTURE_TAG_TAG_ID_COLUMN_NAME + "=?");
-		_selectPicturesByTag = _connection.prepareStatement("select "
-				+ PICTURE_TAG_PICTURE_ID_COLUMN_NAME + " FROM "
-				+ PICTURE_TAG_TABLE_NAME + " WHERE "
-				+ PICTURE_TAG_TAG_ID_COLUMN_NAME + "=?");
-		_countPictures = _connection.prepareStatement("select count(" + "TODO"
-				+ ") FROM " + PICTURE_TABLE_NAME);
-
 		if (!checkAndCreateDirectories())
 		{
 			throw new IOException(
 					"Error creating the required picture directories : "
 							+ _conf.getPictureDirectory());
 		}
-		createTables();
+		_fileListConnection.createTables();
 		loadTags();
 	}
 
@@ -282,7 +190,7 @@ public class SQLFileDataSource implements IDataSource
 			_tagMap.put(tagId, newTag);
 			try
 			{
-				saveTagToDatabase(newTag);
+				_fileListConnection.saveTagToDatabase(newTag);
 			}
 			catch (final SQLException e)
 			{
@@ -298,57 +206,6 @@ public class SQLFileDataSource implements IDataSource
 	}
 
 	/**
-	 * Create the tables of this database.
-	 * 
-	 * @throws SQLException
-	 *             if an SQL error occurred during the creation of the tables.
-	 */
-	private void createTables() throws SQLException
-	{
-		final Statement statement = _connection.createStatement();
-		// Tag table
-		statement.executeUpdate("create table " + TAG_TABLE_NAME
-				+ " if not exists (" + TAG_ID_COLUMN_NAME + " integer, "
-				+ TAG_NAME_COLUMN_NAME + " text, "
-				+ TAG_DESCRIPTION_COLUMN_NAME + " text, "
-				+ TAG_PARENT_ID_COLUMN_NAME + " integer, "
-				+ TAG_SELECTABLE_COLUMN_NAME + " boolean)");
-		// picture table
-
-		// picture_tag table
-		statement.executeUpdate("create table " + PICTURE_TAG_TABLE_NAME
-				+ " if not exists (" + PICTURE_TAG_TAG_ID_COLUMN_NAME
-				+ " integer, " + PICTURE_TAG_PICTURE_ID_COLUMN_NAME
-				+ " integer)");
-	}
-
-	/**
-	 * Save the given {@link Tag} to the database.
-	 * 
-	 * @param tag
-	 *            the {@link Tag} to save.
-	 * @throws SQLException
-	 *             if an SQL error occurred during the saving of the {@link Tag}
-	 *             .
-	 */
-	private void saveTagToDatabase(final Tag tag) throws SQLException
-	{
-		_psInsertTag.setInt(1, tag.getTagId());
-		_psInsertTag.setString(2, tag.getName());
-		_psInsertTag.setString(3, tag.getDescription());
-		if (tag.getParent() == null)
-		{
-			_psInsertTag.setInt(4, -1);
-		}
-		else
-		{
-			_psInsertTag.setInt(4, tag.getParent().getTagId());
-		}
-		_psInsertTag.setBoolean(5, tag.isSelectable());
-		_psInsertTag.executeUpdate();
-	}
-
-	/**
 	 * Load {@link Tag}s from the database.
 	 * 
 	 * @throws SQLException
@@ -357,35 +214,30 @@ public class SQLFileDataSource implements IDataSource
 	 */
 	private void loadTags() throws SQLException
 	{
-		final Statement statementLoad = _connection.createStatement();
-		final ResultSet resLoad = statementLoad.executeQuery("select "
-				+ TAG_ID_COLUMN_NAME + ", " + TAG_NAME_COLUMN_NAME + ", "
-				+ TAG_DESCRIPTION_COLUMN_NAME + ", "
-				+ TAG_SELECTABLE_COLUMN_NAME + " from " + TAG_TABLE_NAME);
+		final ResultSet resLoad = _fileListConnection.loadTagList();
 		Tag tag = null;
 		Tag parentTag = null;
 		while (resLoad.next())
 		{
-			final int iTagId = resLoad.getInt(TAG_ID_COLUMN_NAME);
-			final String strName = resLoad.getString(TAG_NAME_COLUMN_NAME);
+			final int iTagId = resLoad
+					.getInt(SQLFileListConnection.TAG_ID_COLUMN_NAME);
+			final String strName = resLoad
+					.getString(SQLFileListConnection.TAG_NAME_COLUMN_NAME);
 			final String strDescription = resLoad
-					.getString(TAG_DESCRIPTION_COLUMN_NAME);
+					.getString(SQLFileListConnection.TAG_DESCRIPTION_COLUMN_NAME);
 			final boolean bSelectable = resLoad
-					.getBoolean(TAG_SELECTABLE_COLUMN_NAME);
+					.getBoolean(SQLFileListConnection.TAG_SELECTABLE_COLUMN_NAME);
 			tag = new Tag(getId(), iTagId, strName, strDescription, bSelectable);
 			_tagSet.add(tag);
 			_tagMap.put(Integer.valueOf(iTagId), tag);
 		}
-		final Statement statementParent = _connection.createStatement();
-		final ResultSet resParent = statementParent.executeQuery("select "
-				+ TAG_ID_COLUMN_NAME + ", " + TAG_PARENT_ID_COLUMN_NAME
-				+ " from " + TAG_TABLE_NAME);
+		final ResultSet resParent = _fileListConnection.loadParents();
 		while (resParent.next())
 		{
 			final Integer iId = Integer.valueOf(resLoad
-					.getInt(TAG_ID_COLUMN_NAME));
+					.getInt(SQLFileListConnection.TAG_ID_COLUMN_NAME));
 			final Integer iParentId = Integer.valueOf(resLoad
-					.getInt(TAG_PARENT_ID_COLUMN_NAME));
+					.getInt(SQLFileListConnection.TAG_PARENT_ID_COLUMN_NAME));
 			tag = _tagMap.get(iId);
 			parentTag = _tagMap.get(iParentId);
 			if (tag != null && parentTag != null)
---
+++ src/yapto_core/src/yapto/datasource/sqlfile/SQLFileListConnection.java
@@ -0,0 +1,224 @@
+package yapto.datasource.sqlfile;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import yapto.datasource.IPicture;
+import yapto.datasource.sqlfile.config.ISQLFileDataSourceConfiguration;
+import yapto.datasource.tag.Tag;
+
+/**
+ * Object holding the connection to the database and the prepared statements.
+ * 
+ * @author benobiwan
+ * 
+ */
+public class SQLFileListConnection
+{
+	/**
+	 * Configuration for this {@link SQLFileDataSource}.
+	 */
+	private final ISQLFileDataSourceConfiguration _conf;
+
+	/**
+	 * Name for the 'tag' table.
+	 */
+	public static final String TAG_TABLE_NAME = "tag";
+
+	/**
+	 * Name for the 'id' column of the 'tag' table.
+	 */
+	public static final String TAG_ID_COLUMN_NAME = "id";
+
+	/**
+	 * Name for the 'name' column of the 'tag' table.
+	 */
+	public static final String TAG_NAME_COLUMN_NAME = "name";
+
+	/**
+	 * Name for the 'description' column of the 'tag' table.
+	 */
+	public static final String TAG_DESCRIPTION_COLUMN_NAME = "description";
+
+	/**
+	 * Name for the 'parentId' column of the 'tag' table.
+	 */
+	public static final String TAG_PARENT_ID_COLUMN_NAME = "parentId";
+
+	/**
+	 * Name for the 'selectable' column of the 'tag' table.
+	 */
+	public static final String TAG_SELECTABLE_COLUMN_NAME = "selectable";
+
+	/**
+	 * Name for the 'picture' table.
+	 */
+	public static final String PICTURE_TABLE_NAME = "picture";
+
+	/**
+	 * Name for the 'picture_tag' table.
+	 */
+	public static final String PICTURE_TAG_TABLE_NAME = "picture_tag";
+
+	/**
+	 * Name for the 'tagId' column of the 'picture_tag' table.
+	 */
+	public static final String PICTURE_TAG_TAG_ID_COLUMN_NAME = "tagId";
+
+	/**
+	 * Name for the 'pictureId' column of the 'picture_tag' table.
+	 */
+	public static final String PICTURE_TAG_PICTURE_ID_COLUMN_NAME = "pictureId";
+
+	/**
+	 * Connection to the database.
+	 */
+	private final Connection _connection;
+
+	/**
+	 * Statement to insert a {@link Tag}.
+	 */
+	private final PreparedStatement _psInsertTag;
+
+	/**
+	 * Statement to count the number of {@link IPicture}s having a given
+	 * {@link Tag}.
+	 */
+	private final PreparedStatement _countPicturesByTag;
+
+	/**
+	 * Statement to count the total number of {@link IPicture}s in this
+	 * {@link SQLFileDataSource}.
+	 */
+	private final PreparedStatement _countPictures;
+
+	/**
+	 * Statement to select all the {@link IPicture}s having a given {@link Tag}.
+	 */
+	private final PreparedStatement _selectPicturesByTag;
+
+	/**
+	 * creates a new SQLFileListConnection.
+	 * 
+	 * @param conf
+	 *            configuration for this {@link SQLFileListConnection}.
+	 * @throws SQLException
+	 *             if an SQL error occurred during the connection to the
+	 *             database.
+	 * @throws ClassNotFoundException
+	 *             if the database driver class can't be found.
+	 */
+	public SQLFileListConnection(final ISQLFileDataSourceConfiguration conf)
+			throws ClassNotFoundException, SQLException
+	{
+		_conf = conf;
+		Class.forName("org.sqlite.JDBC");
+		_connection = DriverManager.getConnection("jdbc:sqlite:"
+				+ _conf.getDatabaseFileName());
+		_psInsertTag = _connection.prepareStatement("insert into "
+				+ TAG_TABLE_NAME + " (" + TAG_ID_COLUMN_NAME + ", "
+				+ TAG_NAME_COLUMN_NAME + ", " + TAG_DESCRIPTION_COLUMN_NAME
+				+ ", " + TAG_PARENT_ID_COLUMN_NAME + ", "
+				+ TAG_SELECTABLE_COLUMN_NAME + ") values(?, ?, ?, ?, ?)");
+		_countPicturesByTag = _connection.prepareStatement("select count("
+				+ PICTURE_TAG_PICTURE_ID_COLUMN_NAME + ") FROM "
+				+ PICTURE_TAG_TABLE_NAME + " WHERE "
+				+ PICTURE_TAG_TAG_ID_COLUMN_NAME + "=?");
+		_selectPicturesByTag = _connection.prepareStatement("select "
+				+ PICTURE_TAG_PICTURE_ID_COLUMN_NAME + " FROM "
+				+ PICTURE_TAG_TABLE_NAME + " WHERE "
+				+ PICTURE_TAG_TAG_ID_COLUMN_NAME + "=?");
+		_countPictures = _connection.prepareStatement("select count(" + "TODO"
+				+ ") FROM " + PICTURE_TABLE_NAME);
+	}
+
+	/**
+	 * Save the given {@link Tag} to the database.
+	 * 
+	 * @param tag
+	 *            the {@link Tag} to save.
+	 * @throws SQLException
+	 *             if an SQL error occurred during the saving of the {@link Tag}
+	 *             .
+	 */
+	public void saveTagToDatabase(final Tag tag) throws SQLException
+	{
+		_psInsertTag.setInt(1, tag.getTagId());
+		_psInsertTag.setString(2, tag.getName());
+		_psInsertTag.setString(3, tag.getDescription());
+		if (tag.getParent() == null)
+		{
+			_psInsertTag.setInt(4, -1);
+		}
+		else
+		{
+			_psInsertTag.setInt(4, tag.getParent().getTagId());
+		}
+		_psInsertTag.setBoolean(5, tag.isSelectable());
+		_psInsertTag.executeUpdate();
+	}
+
+	/**
+	 * Create the tables of this database.
+	 * 
+	 * @throws SQLException
+	 *             if an SQL error occurred during the creation of the tables.
+	 */
+	public void createTables() throws SQLException
+	{
+		final Statement statement = _connection.createStatement();
+		// Tag table
+		statement.executeUpdate("create table " + TAG_TABLE_NAME
+				+ " if not exists (" + TAG_ID_COLUMN_NAME + " integer, "
+				+ TAG_NAME_COLUMN_NAME + " text, "
+				+ TAG_DESCRIPTION_COLUMN_NAME + " text, "
+				+ TAG_PARENT_ID_COLUMN_NAME + " integer, "
+				+ TAG_SELECTABLE_COLUMN_NAME + " boolean)");
+		// picture table
+
+		// picture_tag table
+		statement.executeUpdate("create table " + PICTURE_TAG_TABLE_NAME
+				+ " if not exists (" + PICTURE_TAG_TAG_ID_COLUMN_NAME
+				+ " integer, " + PICTURE_TAG_PICTURE_ID_COLUMN_NAME
+				+ " integer)");
+	}
+
+	/**
+	 * Load the list of tags.
+	 * 
+	 * @return a {@link ResultSet} containing the list of tags.
+	 * @throws SQLException
+	 *             if an SQL error occurred during the interrogation of the
+	 *             database.
+	 */
+	public ResultSet loadTagList() throws SQLException
+	{
+		final Statement statementLoad = _connection.createStatement();
+		return statementLoad
+				.executeQuery("select " + TAG_ID_COLUMN_NAME + ", "
+						+ TAG_NAME_COLUMN_NAME + ", "
+						+ TAG_DESCRIPTION_COLUMN_NAME + ", "
+						+ TAG_SELECTABLE_COLUMN_NAME + " from "
+						+ TAG_TABLE_NAME);
+	}
+
+	/**
+	 * Load the list of tag id and there parent id.
+	 * 
+	 * @return a {@link ResultSet} containing the list of tag id and there
+	 *         parent id.
+	 * @throws SQLException
+	 *             if an SQL error occurred during the interrogation of the
+	 *             database.
+	 */
+	public ResultSet loadParents() throws SQLException
+	{
+		final Statement statementParent = _connection.createStatement();
+		return statementParent.executeQuery("select " + TAG_ID_COLUMN_NAME
+				+ ", " + TAG_PARENT_ID_COLUMN_NAME + " from " + TAG_TABLE_NAME);
+	}
+}
