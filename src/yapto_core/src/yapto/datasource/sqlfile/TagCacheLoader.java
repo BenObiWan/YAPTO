9f28338dda9e9252fa7a10821fd12c9aeebeab8a
BenObiWan <benobiwan@gmail.com>
2012-03-05 00:04:47 +0100

    Added TagCacheLoader.

---
+++ src/yapto_core/src/yapto/datasource/sqlfile/TagCacheLoader.java
@@ -0,0 +1,56 @@
+package yapto.datasource.sqlfile;
+
+import java.sql.ResultSet;
+
+import yapto.datasource.sqlfile.config.FsPictureCacheLoaderConfiguration;
+import yapto.datasource.tag.Tag;
+
+import com.google.common.cache.CacheLoader;
+
+/**
+ * A {@link CacheLoader} loading {@link Tag} from a database.
+ * 
+ * @author benobiwan
+ * 
+ */
+public final class TagCacheLoader extends CacheLoader<Integer, Tag>
+{
+	/**
+	 * The configuration of this FsPictureCacheLoader.
+	 */
+	private final FsPictureCacheLoaderConfiguration _conf;
+
+	/**
+	 * Object holding the connection to the database and the prepared
+	 * statements.
+	 */
+	private final SQLFileListConnection _fileListConnection;
+
+	/**
+	 * Creates a new TagCacheLoader.
+	 * 
+	 * @param conf
+	 *            the configuration for this FsPictureCacheLoader.
+	 * @param fileListConnection
+	 *            object holding the connection to the database and the prepared
+	 *            statements.
+	 */
+	public TagCacheLoader(final FsPictureCacheLoaderConfiguration conf,
+			final SQLFileListConnection fileListConnection)
+	{
+		_conf = conf;
+		_fileListConnection = fileListConnection;
+	}
+
+	@Override
+	public Tag load(final Integer key) throws Exception
+	{
+		final ResultSet res = _fileListConnection.loadTag(key.intValue());
+		return new Tag(
+				_conf.getDataSourceId(),
+				res.getInt(SQLFileListConnection.TAG_ID_COLUMN_NAME),
+				res.getString(SQLFileListConnection.TAG_NAME_COLUMN_NAME),
+				res.getString(SQLFileListConnection.TAG_DESCRIPTION_COLUMN_NAME),
+				res.getBoolean(SQLFileListConnection.TAG_SELECTABLE_COLUMN_NAME));
+	}
+}
