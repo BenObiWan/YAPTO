08c88daa3642d62cd59a11e8a9646308ddc847bf
BenObiWan <benobiwan@gmail.com>
2012-08-03 22:24:26 +0200

    Added ImageLoader to load image in different sizes.

---
+++ src/yapto_core/src/yapto/datasource/sqlfile/ImageLoader.java
@@ -0,0 +1,88 @@
+package yapto.datasource.sqlfile;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import yapto.datasource.IPicture;
+import yapto.datasource.sqlfile.config.ISQLFileDataSourceConfiguration;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+public final class ImageLoader
+{
+	/**
+	 * {@link LoadingCache} used to load the {@link BufferedImage}.
+	 */
+	private final LoadingCache<String, BufferedImage> _mainImageCache;
+
+	/**
+	 * {@link LoadingCache} used to load the {@link BufferedImage}.
+	 */
+	private final LoadingCache<String, BufferedImage> _thumbnailCache;
+
+	/**
+	 * Creates a new ImageLoader.
+	 * 
+	 * @param conf
+	 *            the configuration of this {@link ImageLoader}.
+	 */
+	public ImageLoader(final ISQLFileDataSourceConfiguration conf)
+	{
+		// image cache
+		final CacheLoader<String, BufferedImage> imageLoader = new BufferedImageCacheLoader(
+				conf.getMainPictureLoaderConfiguration());
+		_mainImageCache = CacheBuilder.newBuilder().build(imageLoader);
+
+		// thumbnail cache
+		final CacheLoader<String, BufferedImage> thumbnailLoader = new BufferedImageCacheLoader(
+				conf.getThumbnailPictureLoaderConfiguration());
+		_thumbnailCache = CacheBuilder.newBuilder().build(thumbnailLoader);
+	}
+
+	/**
+	 * Get the data of the main image of the specified {@link IPicture}.
+	 * 
+	 * @param strId
+	 *            the id of the image to get.
+	 * @return the data of the main image of the specified {@link IPicture}.
+	 * @throws IOException
+	 *             if an error occurs during reading.
+	 */
+	public BufferedImage getImageData(final String strId) throws IOException
+	{
+		try
+		{
+			return _mainImageCache.get(strId);
+		}
+		catch (final ExecutionException e)
+		{
+			throw new IOException(e);
+		}
+	}
+
+	/**
+	 * Get the data of the thumbnail image of the specified {@link IPicture}.
+	 * 
+	 * @param strId
+	 *            the id of the image to get.
+	 * @return the data of the thumbnail image of the specified {@link IPicture}
+	 *         .
+	 * @throws IOException
+	 *             if an error occurs during reading.
+	 */
+	public BufferedImage getThumbnailData(final String strId)
+			throws IOException
+	{
+		try
+		{
+			return _thumbnailCache.get(strId);
+		}
+		catch (final ExecutionException e)
+		{
+			throw new IOException(e);
+		}
+	}
+}
