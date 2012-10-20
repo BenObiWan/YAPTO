50d61a7fdb6851949953bb905819d240f899c746
BenObiWan <benobiwan@gmail.com>
2012-10-21 01:01:35 +0200

    Add or remove tag from pictures when they are selected.

--- src/yapto_swing/src/yapto/swing/SelectingTreeTagPanel.java
+++ src/yapto_swing/src/yapto/swing/SelectingTreeTagPanel.java
@@ -1,12 +1,17 @@
 package yapto.swing;
 
+import it.cnr.imaa.essi.lablib.gui.checkboxtree.TreeCheckingEvent;
+import it.cnr.imaa.essi.lablib.gui.checkboxtree.TreeCheckingListener;
 import it.cnr.imaa.essi.lablib.gui.checkboxtree.TreeCheckingModel.CheckingMode;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
 import yapto.datasource.IPicture;
 import yapto.datasource.IPictureBrowser;
+import yapto.datasource.tag.ITag;
 
 public class SelectingTreeTagPanel extends AbstractTreeTagPanel
 {
-
 	/**
 	 * serialVersionUID for Serialization.
 	 */
@@ -23,6 +28,28 @@ public class SelectingTreeTagPanel extends AbstractTreeTagPanel
 	{
 		super(pictureIterator);
 		_tagTree.getCheckingModel().setCheckingMode(CheckingMode.SIMPLE);
+		_tagTree.addTreeCheckingListener(new TreeCheckingListener()
+		{
+			@Override
+			public void valueChanged(final TreeCheckingEvent e)
+			{
+				final Object value = e.getPath().getLastPathComponent();
+				final Object userObject = ((DefaultMutableTreeNode) value)
+						.getUserObject();
+				if (userObject != null && userObject instanceof ITag)
+				{
+					final ITag tag = (ITag) userObject;
+					final IPicture pic = _pictureIterator.getCurrentPicture();
+					if (e.isCheckedPath())
+					{
+						pic.addTag(tag);
+					}
+					else
+					{
+						pic.removeTag(tag);
+					}
+				}
+			}
+		});
 	}
-
 }
