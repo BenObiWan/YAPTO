e73f9a6eb1ed6e1e0397818a873a44dc3fdfe1eb
BenObiWan <benobiwan@gmail.com>
2012-11-23 16:41:17 +0100

    Added PictureBank name to IPictureBankConfiguration.

--- src/yapto_core/src/yapto/picturebank/config/IPictureBankConfiguration.java
+++ src/yapto_core/src/yapto/picturebank/config/IPictureBankConfiguration.java
@@ -18,9 +18,21 @@ public interface IPictureBankConfiguration extends IConfigurationBranch
 	String PICTUREBANK_ID_TAG = "PictureBankId";
 
 	/**
+	 * Tag for the {@link IPictureBank} name.
+	 */
+	String PICTUREBANK_NAME_TAG = "PictureBankName";
+
+	/**
 	 * Get the id of this {@link IPictureBank}.
 	 * 
 	 * @return the id of this {@link IPictureBank}.
 	 */
 	int getPictureBankId();
+
+	/**
+	 * Get the name of this {@link IPictureBank}.
+	 * 
+	 * @return the name of this {@link IPictureBank}.
+	 */
+	String getPictureBankName();
 }
