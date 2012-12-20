74fba248b039ba8732241f11cabb286787aa2d0f
BenObiWan <benobiwan@gmail.com>
2012-12-20 20:59:59 +0100

    Added FilteringTreeTagPanel.

---
+++ src/yapto_swing/src/yapto/swing/FilteringTreeTagPanel.java
@@ -0,0 +1,89 @@
+package yapto.swing;
+
+import it.cnr.imaa.essi.lablib.gui.checkboxtree.TreeCheckingModel.CheckingMode;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+
+import yapto.picturebank.IPicture;
+import yapto.picturebank.IPictureBrowser;
+import yapto.picturebank.tag.ITag;
+import yapto.picturebank.tag.TagRepositoryChangedEvent;
+
+import com.google.common.eventbus.Subscribe;
+
+/**
+ * Implementation of {@link AbstractTreeTagPanel} where multiple {@link ITag}s
+ * can be selected.
+ * 
+ * Used to filter {@link IPicture}s to display by {@link ITag}s.
+ * 
+ * @author benobiwan
+ * 
+ */
+public class FilteringTreeTagPanel extends AbstractTreeTagPanel
+{
+	/**
+	 * serialVersionUID for Serialization.
+	 */
+	private static final long serialVersionUID = 5743908692336875510L;
+
+	/**
+	 * Creates a new SelectingTreeTagPanel.
+	 * 
+	 * @param pictureIterator
+	 *            the {@link IPictureBrowser} to use.
+	 */
+	public FilteringTreeTagPanel(
+			final IPictureBrowser<? extends IPicture> pictureIterator)
+	{
+		super(pictureIterator);
+		_tagTree.getCheckingModel().setCheckingMode(CheckingMode.SIMPLE);
+	}
+
+	/**
+	 * Clear checking of all the {@link ITag}s.
+	 */
+	public void unsetSelectedTags()
+	{
+		_tagTree.clearChecking();
+	}
+
+	@Override
+	@Subscribe
+	public void handleTagRepositoryChangedEvent(
+			final TagRepositoryChangedEvent ev)
+	{
+		super.handleTagRepositoryChangedEvent(ev);
+	}
+
+	/**
+	 * Get the ids of the selected {@link ITag}s.
+	 * 
+	 * @return the ids of the selected {@link ITag}s.
+	 */
+	public int[] getSelectedTagIds()
+	{
+		final TreePath[] paths = _tagTree.getCheckingPaths();
+		final int[] tagIds = new int[paths.length];
+		int i = 0;
+		for (final TreePath path : paths)
+		{
+			if (path != null)
+			{
+				final Object sel = path.getLastPathComponent();
+				if (sel != null && sel instanceof DefaultMutableTreeNode)
+				{
+					final Object userObject = ((DefaultMutableTreeNode) sel)
+							.getUserObject();
+					if (userObject != null && userObject instanceof ITag)
+					{
+						tagIds[i] = ((ITag) userObject).getTagId();
+						i++;
+					}
+				}
+			}
+		}
+		return tagIds;
+	}
+}
