e1779d41214daf2a5c646d139949e24103eaa877
BenObiWan <benobiwan@gmail.com>
2012-12-20 21:19:28 +0100

    Added getSelectedTags method.

--- src/yapto_swing/src/yapto/swing/FilteringTreeTagPanel.java
+++ src/yapto_swing/src/yapto/swing/FilteringTreeTagPanel.java
@@ -75,4 +75,34 @@ public class FilteringTreeTagPanel extends AbstractTreeTagPanel
 		}
 		return tagIds;
 	}
+
+	/**
+	 * Get the selected {@link ITag}s.
+	 * 
+	 * @return the selected {@link ITag}s.
+	 */
+	public ITag[] getSelectedTags()
+	{
+		final TreePath[] paths = _tagTree.getCheckingPaths();
+		final ITag[] tags = new ITag[paths.length];
+		int i = 0;
+		for (final TreePath path : paths)
+		{
+			if (path != null)
+			{
+				final Object sel = path.getLastPathComponent();
+				if (sel != null && sel instanceof DefaultMutableTreeNode)
+				{
+					final Object userObject = ((DefaultMutableTreeNode) sel)
+							.getUserObject();
+					if (userObject != null && userObject instanceof ITag)
+					{
+						tags[i] = (ITag) userObject;
+						i++;
+					}
+				}
+			}
+		}
+		return tags;
+	}
 }
