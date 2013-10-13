6dccba6f6b651754c3bf3684288cb7031741ffdd
BenObiWan <benobiwan@gmail.com>
2013-10-13 22:38:28 +0200

    Javadoc.

--- src/yapto_core/src/yapto/picturebank/PictureListWithIndex.java
+++ src/yapto_core/src/yapto/picturebank/PictureListWithIndex.java
@@ -2,23 +2,53 @@ package yapto.picturebank;
 
 import java.util.List;
 
+/**
+ * List of picture Ids plus index of the selected picture.
+ * 
+ * @author benobiwan
+ */
 public final class PictureListWithIndex
 {
-	private final List<String> _list;
+	/**
+	 * List of picture Ids.
+	 */
+	private final List<String> _pictureIdList;
 
+	/**
+	 * Index of the current picture.
+	 */
 	private final int _iIndex;
 
-	public PictureListWithIndex(final List<String> list, final int iIndex)
+	/**
+	 * Create a new picture list with index.
+	 * 
+	 * @param pictureIdList
+	 *            list of picture Ids.
+	 * @param iIndex
+	 *            index of the current picture.
+	 */
+	public PictureListWithIndex(final List<String> pictureIdList,
+			final int iIndex)
 	{
-		_list = list;
+		_pictureIdList = pictureIdList;
 		_iIndex = iIndex;
 	}
 
-	public List<String> getPictureList()
+	/**
+	 * Get the list of picture Ids.
+	 * 
+	 * @return the list of picture Ids.
+	 */
+	public List<String> getPictureIdList()
 	{
-		return _list;
+		return _pictureIdList;
 	}
 
+	/**
+	 * Get the index of the currently selected picture.
+	 * 
+	 * @return the index of the currently selected picture.
+	 */
 	public int getIndex()
 	{
 		return _iIndex;
