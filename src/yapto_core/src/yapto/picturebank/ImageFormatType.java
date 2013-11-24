896ac8e6d454abeed6513706fc3563647d65ca27
BenObiWan <benobiwan@gmail.com>
2013-11-24 03:09:55 +0100

    Added unknown picture format. Added ImageMagick description of the picture format.

--- src/yapto_core/src/yapto/picturebank/ImageFormatType.java
+++ src/yapto_core/src/yapto/picturebank/ImageFormatType.java
@@ -10,41 +10,65 @@ public enum ImageFormatType
 	/**
 	 * JPG image file.
 	 */
-	JPG("jpg"),
+	JPG("JPG", "JPEG (Joint Photographic Experts Group JFIF format)"),
 
 	/**
 	 * PNG image file.
 	 */
-	PNG("png"),
+	PNG("PNG", "PNG (Portable Network Graphics)"),
 
 	/**
 	 * CR2 image file.
 	 */
-	CR2("cr2");
+	CR2("CR2", "CR2 (Canon Digital Camera Raw Image Format)"),
 
 	/**
-	 * Message for this exception type.
+	 * Unknown image file.
+	 */
+	UNKNOWN("", "");
+
+	/**
+	 * Extension of this image format.
 	 */
 	private final String _strExtension;
+
+	/**
+	 * ImageMagick description of this image format.
+	 */
+	private final String _strImageMagickIdentify;
 
 	/**
 	 * Creates a new PictureAddExceptionType.
 	 * 
 	 * @param strExtension
-	 *            message for this exception type.
+	 *            extension of this image format.
+	 * @param strImageMagickIdentify
+	 *            ImageMagick description of this image format.
 	 */
-	private ImageFormatType(final String strExtension)
+	private ImageFormatType(final String strExtension,
+			final String strImageMagickIdentify)
 	{
 		_strExtension = strExtension;
+		_strImageMagickIdentify = strImageMagickIdentify;
 	}
 
 	/**
-	 * Get the message for this exception type.
+	 * Get the extension of this image format.
 	 * 
-	 * @return the message for this exception type.
+	 * @return the extension of this image format.
 	 */
 	public String getExtension()
 	{
 		return _strExtension;
+	}
+
+	/**
+	 * Get the ImageMagick description of this image format.
+	 * 
+	 * @return the ImageMagick description of this image format.
+	 */
+	public String getImageMagickIdentify()
+	{
+		return _strImageMagickIdentify;
 	}
 }
